406286ad1d977f0106b8a1d3786764fabd406f91
Milenko Mitrovic <subzone@live.com>
2026-05-03 22:00:24 +0200

    Add m formula v0.0.18

---
+++ Formula/m.rb
@@ -0,0 +1,19 @@
+class M < Formula
+  desc "MD-driven agent CLI for code, infrastructure, and automation"
+  homepage "https://github.com/subzone/Agentctl"
+  url "https://github.com/subzone/Agentctl/archive/refs/tags/v0.0.18.tar.gz"
+  sha256 :no_check
+  license "MIT"
+  head "https://github.com/subzone/Agentctl.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "-o", bin/"m", "./cmd/m"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/m --version")
+  end
+end
